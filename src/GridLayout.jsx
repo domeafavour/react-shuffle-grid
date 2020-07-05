a1d3cba22ba92ddc73e73c1315a8e0379b3d917d
zhanghaojie <zhanghaojie@midaigroup.com>
2020-07-06 00:01:54 +0800

    add: jsconfig.json file for vscode.
    - and /src alias

--- src/GridLayout.jsx
+++ src/GridLayout.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './GridLayout.module.css';
 import { getPosition } from './utils';
-import Card from './components/Card';
+import Card from '@/components/Card';
 
 const cols = 3;
 
@@ -16,23 +16,23 @@ export const GirdItem = React.memo(({ children, index, duration, flipped, setFli
         transition: transform ${duration}ms;
         transform: translate(${(1 - x) * 160}px, ${(1 - y) * 160}px);
         z-index: ${index};
-        animation-delay: ${Math.random()*1000}ms;
+        animation-delay: ${Math.random() * 1000}ms;
       `;
     } else {
       domRef.current.style.transform = `translate(0, 0)`;
     }
-  }, [index, shuffling, domRef]);
+  }, [index, shuffling, domRef, duration]);
 
   useEffect(() => {
     setFlipped(index, true);
   }, [index, setFlipped]);
 
   const { x, y } = getPosition(index, cols);
-  
+
   return (
     <div
       ref={domRef}
-      className={[styles.gridItem, shuffling ? styles.shuffling: false].filter(Boolean).join(' ')}
+      className={[styles.gridItem, shuffling ? styles.shuffling : false].filter(Boolean).join(' ')}
       onClick={() => setFlipped(index, !flipped)}
     >
       <Card
@@ -44,7 +44,6 @@ export const GirdItem = React.memo(({ children, index, duration, flipped, setFli
         )}
         flipped={flipped}
       />
-
     </div>
   );
 });
